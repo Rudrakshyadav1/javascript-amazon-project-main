ffa4250dd26518940716191803e95ef7f056a81b
Rudrakshyadav1 <yadavrudraksh19@gmail.com>
2024-10-01 08:23:06 +0530

    Initial commit

---
+++ js-amazon/amazon.js
@@ -0,0 +1,93 @@
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
+let html = ``;
+
+products.forEach((product) => {
+  html += `
+  <div class="product-container">
+    <div class="product-image-container">
+      <img class="product-image"
+        src="${product.image}">
+    </div>
+
+    <div class="product-name limit-text-to-2-lines">
+      ${product.name}
+    </div>
+
+    <div class="product-rating-container">
+      <img class="product-rating-stars"
+        src="images/ratings/rating-${product.rating.stars * 10}.png">
+      <div class="product-rating-count link-primary">
+        87
+      </div>
+    </div>
+
+    <div class="product-price">
+      $${(product.priceCents / 100).toFixed(2)}
+    </div>
+
+    <div class="product-quantity-container">
+      <select>
+        <option selected value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+
+    <div class="product-spacer"></div>
+
+    <div class="added-to-cart">
+      <img src="images/icons/checkmark.png">
+      Added
+    </div>
+
+    <button class="add-to-cart-button button-primary" data-product-id=${product.id}>
+      Add to Cart
+    </button>
+  </div>
+`});
+
+document.querySelector(".products-grid").innerHTML = html;
+
+document.querySelectorAll(".add-to-cart-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    addtocart(productId);
+    updateCartQuantity()
+
+  });
+});
+function updateCartQuantity(){
+  let sum = 0;
+  cart.forEach((item) => {
+    sum += item.quantity;
+  });
+  console.log("total quantity =", sum);
+  document.querySelector(".cart-quantity").innerHTML = `${sum}`;
+  console.log(cart);
+
+}
+function addtocart(productId) {
+  let matchingItem;
+  cart.forEach((item) => {
+    if (productId === item.productId) {
+      matchingItem = item;
+    }
+  });
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+}
